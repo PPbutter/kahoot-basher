33d46b3fd10d0ba5f12b55707c64e0937f164a0a
poiper <rashow3377@gmail.com>
2022-01-06 16:15:12 -0500

    version : 0.0.1 | "well this is stoopid"

---
+++ main.js
@@ -0,0 +1,31 @@
+console.clear();
+const kooter = require('kahoot-toolkit');
+const readline = require('readline-sync');
+const colors = require('colors');
+
+let p = new kooter()
+p.pin = readline.question(`Enter the ${(colors.bold("Kahoot").magenta)} pin: `);
+
+console.log((`\n${colors.bold("╭─").red}${colors.bold("OPTIONS").white}${colors.bold("───").red}${colors.bold("────╮").red}`));
+console.log(colors.bold("│").red,colors.bold('F').cyan,colors.bold(":").white,colors.bold('Flood').magenta,colors.bold("    │").red);
+console.log(colors.bold("│").red,colors.bold('A').cyan,colors.bold(":").white,colors.bold('Autoplay').magenta,colors.bold(" │").red);
+console.log(colors.bold("╰───────────────╯").red) 
+
+let mode = readline.question(`\n${colors.bold("select mode").white} ${colors.bold("(").white}${colors.bold('F').cyan}${colors.bold("/").white}${colors.bold('A').cyan}${colors.bold(")").white}${colors.bold(" >").magenta} `)
+
+if (mode.toLowerCase() == "f"){
+    let amount = readline.question(`\n${colors.bold("amount of bots").white} ${colors.bold(" >").magenta} `)
+    let name = readline.question(`\n${colors.bold("name for bots").white} ${colors.bold(" >").magenta} `).replace(/a/g, 'ᗩ').replace(/b/g, 'ᗷ').replace(/c/g, 'ᑕ').replace(/d/g, 'ᗪ').replace(/e/g, 'E').replace(/f/g, 'ᖴ').replace(/g/g, 'G').replace(/h/g, 'ᕼ').replace(/i/g, 'I').replace(/j/g, 'ᒍ').replace(/k/g, 'K').replace(/l/g, 'ᒪ').replace(/m/g, 'ᗰ').replace(/n/g, 'ᑎ').replace(/o/g, 'O').replace(/p/g, 'ᑭ').replace(/q/g, 'ᑫ').replace(/r/g, 'ᖇ').replace(/s/g, 'ᔕ').replace(/t/g, 'T').replace(/u/g, 'ᑌ').replace(/v/g, 'ᐯ').replace(/w/g, 'ᗯ').replace(/x/g, '᙭').replace(/y/g, 'Y').replace(/z/g, 'ᘔ').replace(/A/g, 'ᗩ').replace(/B/g, 'ᗷ').replace(/C/g, 'ᑕ').replace(/D/g, 'ᗪ').replace(/E/g, 'E').replace(/F/g, 'ᖴ').replace(/G/g, 'G').replace(/H/g, 'ᕼ').replace(/I/g, 'I').replace(/J/g, 'ᒍ').replace(/K/g, 'K').replace(/L/g, 'ᒪ').replace(/M/g, 'ᗰ').replace(/N/g, 'ᑎ').replace(/O/g, 'O').replace(/P/g, 'ᑭ').replace(/Q/g, 'ᑫ').replace(/R/g, 'ᖇ').replace(/S/g, 'ᔕ').replace(/T/g, 'T').replace(/U/g, 'ᑌ').replace(/V/g, 'ᐯ').replace(/W/g, 'ᗯ').replace(/X/g, '᙭').replace(/Y/g, 'Y').replace(/Z/g, 'ᘔ')
+    p.name = name;
+    p.flood(amount=amount,persistent=false).catch((dat)=>{
+        console.log(colors.bold("\n[-]").red,colors.bold(dat).red)
+    })
+}
+if (mode.toLowerCase() == 'a'){
+    let quiz = readline.question(`\n${colors.bold("Enter The ID or Name of the Quiz to Join.").white} ${colors.bold(" >").magenta} `)
+    let name = readline.question(`\n${colors.bold("name for bot").white} ${colors.bold(" >").magenta} `).replace(/a/g, 'ᗩ').replace(/b/g, 'ᗷ').replace(/c/g, 'ᑕ').replace(/d/g, 'ᗪ').replace(/e/g, 'E').replace(/f/g, 'ᖴ').replace(/g/g, 'G').replace(/h/g, 'ᕼ').replace(/i/g, 'I').replace(/j/g, 'ᒍ').replace(/k/g, 'K').replace(/l/g, 'ᒪ').replace(/m/g, 'ᗰ').replace(/n/g, 'ᑎ').replace(/o/g, 'O').replace(/p/g, 'ᑭ').replace(/q/g, 'ᑫ').replace(/r/g, 'ᖇ').replace(/s/g, 'ᔕ').replace(/t/g, 'T').replace(/u/g, 'ᑌ').replace(/v/g, 'ᐯ').replace(/w/g, 'ᗯ').replace(/x/g, '᙭').replace(/y/g, 'Y').replace(/z/g, 'ᘔ').replace(/A/g, 'ᗩ').replace(/B/g, 'ᗷ').replace(/C/g, 'ᑕ').replace(/D/g, 'ᗪ').replace(/E/g, 'E').replace(/F/g, 'ᖴ').replace(/G/g, 'G').replace(/H/g, 'ᕼ').replace(/I/g, 'I').replace(/J/g, 'ᒍ').replace(/K/g, 'K').replace(/L/g, 'ᒪ').replace(/M/g, 'ᗰ').replace(/N/g, 'ᑎ').replace(/O/g, 'O').replace(/P/g, 'ᑭ').replace(/Q/g, 'ᑫ').replace(/R/g, 'ᖇ').replace(/S/g, 'ᔕ').replace(/T/g, 'T').replace(/U/g, 'ᑌ').replace(/V/g, 'ᐯ').replace(/W/g, 'ᗯ').replace(/X/g, '᙭').replace(/Y/g, 'Y').replace(/Z/g, 'ᘔ')
+    p.name = name;
+    p.autoplay(quiz,true).catch((dat)=>{
+        console.log(colors.bold("\n[-]").red,colors.bold(dat).red)
+    })
+}
